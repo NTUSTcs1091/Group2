47dfb0262259c918ecf0d58074a4c2105c0cabaf
zhouyuan <circle.z.yuan@gmail.com>
2021-05-06 16:19:23 +0800

    Update http packet header files

--- src/HttpResponsePacket.h
+++ src/HttpResponsePacket.h
@@ -1,9 +1,12 @@
+// Copyright (c) 2021 Pei-Ru Wang, Cheryl Huang, Yuan Zhou. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /**
  * Project MyHttpServer
  * @author Pei-Ru Wang, Cheryl Huang, Yuan Zhou
  * @version 0.0.1
  */
-
 
 #ifndef _HTTPRESPONSEPACKET_H
 #define _HTTPRESPONSEPACKET_H
@@ -13,16 +16,16 @@
 #include <string>
 #include <vector>
 
- // Encapsulate http response packets.
-class HttpResponsePacket: public HttpPacket {
-public: 
-    std::string protocol;
-    int status_code;
-    
-/**
- * @param response
- */
-static std::vector<char> GetBytes(HttpResponsePacket response);
+// Encapsulate http response packets.
+class HttpResponsePacket : public HttpPacket {
+public:
+  // The protocol of the http response
+  std::string protocol;
+  // The status code of the http response
+  int status_code;
+
+  // Parse a http response packet into bytes
+  static std::vector<char> GetBytes(HttpResponsePacket response);
 };
 
 #endif //_HTTPRESPONSEPACKET_H
