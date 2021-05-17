393daf2363fc7a38b6d015952ec65726249ea785
zhouyuan <circle.z.yuan@gmail.com>
2021-05-17 12:24:58 +0800

    http handler implementation

--- src/HttpHandler.cpp
+++ src/HttpHandler.cpp
@@ -8,28 +8,16 @@
  * @version 0.0.1
  */
 
-
 #include "HttpHandler.h"
 
 /**
  * HttpHandler implementation
  */
 
-HttpHandler::HttpHandler(boost::asio::io_service& io_service) : socket(io_service) {
+HttpHandler::HttpHandler(boost::asio::io_service &io_service)
+    : socket(io_service) {}
+
+std::unique_ptr<boost::asio::ip::tcp::socket> HttpHandler::GetSocket() {
+  return std::make_unique<boost::asio::ip::tcp::socket>(socket);
 }
 
-
-/**
- * @param input
- * @return HttpPacket
- */
-HttpPacket HttpHandler::Handle(HttpPacket input) {
-    return null;
-}
-
-/**
- * @param socket
- */
-void HttpHandler::HttpHandler(socket socket) {
-
-}
--- src/HttpRequestHandler.cpp
+++ src/HttpRequestHandler.cpp
@@ -10,16 +10,61 @@
 
 #include "HttpRequestHandler.h"
 
+#include "HttpRequestPacket.h"
+#include "HttpResponsePacket.h"
+
 /**
  * HttpRequestHandler implementation
  */
 
 HttpRequestHandler::HttpRequestHandler(
     boost::asio::io_service &io_service,
-    std::unordered_map<int, handler_ptr> &mapHandlerList, int nCount)
-    : mapHandlerList(mapHandlerList), nCount(nCount), HttpHandler(io_service) {}
+    std::unordered_map<int, handler_ptr> &map_handler_list, int n_count)
+    : map_handler_list(map_handler_list), n_count(n_count),
+      HttpHandler(io_service) {}
 
-/**
- * @param socket
- */
-void HttpRequestHandler::HttpRequestHandler(socket socket) {}
+HttpRequestHandler::~HttpRequestHandler() {
+  HttpRequestHandler::Stop();
+  std::cout << n_count << ":~HandlerComplete" << std::endl;
+}
+
+void HttpRequestHandler::Start() {
+  auto self(shared_from_this());
+  map_handler_list.insert(std::make_pair(n_count, self));
+
+  HttpRequestHandler::HandleRead();
+}
+
+void HttpRequestHandler::Stop() {
+  auto it = map_handler_list.find(n_count);
+  map_handler_list.erase(it);
+  socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+  socket.close();
+}
+
+void HttpRequestHandler::HandleRead() {
+  auto self(shared_from_this());
+  socket.async_read_some(
+      boost::asio::buffer(buffer),
+      strand.wrap(
+          [this, self](std::error_code error, std::size_t bytes_transferred) {
+            if (!error) {
+              HttpRequestPacket::parse_result result;
+              std::tie(result, std::ignore) = httpRequestPacket.ParseBytes(
+                  buffer.data(), buffer.data() + bytes_transferred);
+
+              if (result == HttpRequestPacket::success) {
+                httpRouterHandler.RouteHttpRequest(httpRequestPacket,
+                                                   httpResponsePacket);
+                HttpRequestHandler::HandleWrite();
+              } else if (result == HttpRequestPacket::success) {
+
+                HttpRequestHandler::HandleWrite();
+              } else {
+                HttpRequestHandler::HandleRead();
+              }
+            }
+          }));
+}
+
+void HttpRequestHandler::HandleWrite() {}
