45e55a7cfa55d1247f7e9e7b910d3201c499a51a
zhouyuan <circle.z.yuan@gmail.com>
2021-06-30 12:58:17 +0800

    Implement ToString()

--- src/HttpRequestPacket.cpp
+++ src/HttpRequestPacket.cpp
@@ -13,3 +13,27 @@
 /**
  * HttpRequestPacket implementation
  */
+
+namespace misc_strings {
+
+const char name_value_separator[] = {':', ' '};
+const char crlf[] = {'\r', '\n'};
+
+}  // namespace misc_strings
+
+std::string HttpRequestPacket::ToString() { 
+  std::string return_str = "";
+  return_str += method + ' ';
+  return_str += path + ' ';
+  return_str += protocol;
+  return_str += misc_strings::crlf;
+  for (const auto &header : headers) {
+    return_str += header.first;
+    return_str += misc_strings::name_value_separator;
+    return_str += header.second;
+    return_str += misc_strings::crlf;
+  }
+  return_str += misc_strings::crlf;
+  return_str += content;
+  return return_str;
+}
